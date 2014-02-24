a7694a2e8e6032594b4b8ae945b141cf610a1428
Greg <gouwa5700@gmail.com>
2014-02-24 15:17:57 +0800

    add Note model

---
+++ my note/Note.h
@@ -0,0 +1,16 @@
+//
+//  Note.h
+//  my note
+//
+//  Created by Greg Xu on 2/24/14.
+//  Copyright (c) 2014 Greg Xu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Note : NSObject
+
+@property NSString *note;
+@property NSDate *creationDate;
+
+@end
---
+++ my note/Note.m
@@ -0,0 +1,13 @@
+//
+//  Note.m
+//  my note
+//
+//  Created by Greg Xu on 2/24/14.
+//  Copyright (c) 2014 Greg Xu. All rights reserved.
+//
+
+#import "Note.h"
+
+@implementation Note
+
+@end
