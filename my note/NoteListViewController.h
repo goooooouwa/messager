4ab33c650c8cde1797b73fc1cdc7588b02a26c79
Greg <gouwa5700@gmail.com>
2014-02-24 15:25:02 +0800

    show initial notes in note list view

---
+++ my note/NoteListViewController.h
@@ -0,0 +1,13 @@
+//
+//  NoteListViewController.h
+//  my note
+//
+//  Created by Greg Xu on 2/24/14.
+//  Copyright (c) 2014 Greg Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NoteListViewController : UITableViewController
+
+@end
---
+++ my note/NoteListViewController.m
@@ -0,0 +1,134 @@
+//
+//  NoteListViewController.m
+//  my note
+//
+//  Created by Greg Xu on 2/24/14.
+//  Copyright (c) 2014 Greg Xu. All rights reserved.
+//
+
+#import "NoteListViewController.h"
+#import "Note.h"
+
+@interface NoteListViewController ()
+
+@property NSMutableArray *notes;
+
+@end
+
+@implementation NoteListViewController
+
+- (void)loadInitialData
+{
+    Note *note1 = [[Note alloc] init];
+    note1.note = @"Today is a good day.";
+    [self.notes addObject:note1];
+    Note *note2 = [[Note alloc] init];
+    note2.note = @"I'm in a good mood.";
+    [self.notes addObject:note2];
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.notes = [[NSMutableArray alloc] init];
+    [self loadInitialData];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [self.notes count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"ListPrototypeCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    Note *note = [self.notes objectAtIndex:indexPath.row];
+    cell.textLabel.text = note.note;
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
+
+@end
