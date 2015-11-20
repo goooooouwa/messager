962814b3f22ac2dd4e641b97e69a04560e163a64
Greg <greg.xu@activenetwork.com>
2015-11-20 16:50:36 +0800

    add my first UI test

---
+++ my noteUITests/my_noteUITests.m
@@ -0,0 +1,50 @@
+//
+//  my_noteUITests.m
+//  my noteUITests
+//
+//  Created by Greg Xu on 11/20/15.
+//  Copyright © 2015 Greg Xu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface my_noteUITests : XCTestCase
+
+@end
+
+@implementation my_noteUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.navigationBars[@"NoteListView"].buttons[@"Add"] tap];
+    
+    XCUIElement *textView = [[[[[app.otherElements containingType:XCUIElementTypeNavigationBar identifier:@"NewNoteView"] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeTextView].element;
+    [textView tap];
+    [textView typeText:@"abc"];
+    [[[[app.navigationBars[@"NewNoteView"] childrenMatchingType:XCUIElementTypeButton] matchingIdentifier:@"Back"] elementBoundByIndex:0] tap];
+    [app.tables.staticTexts[@"abc"] tap];
+    
+}
+
+@end
